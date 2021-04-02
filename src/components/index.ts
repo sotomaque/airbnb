a212585a6134ffcda0b3bdee9ecc79af449350bb
Enrique <sotomaque@gmail.com>
2021-04-01 18:44:46 -0700

    fix: remove unused boilerplate components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,4 @@
 import CustomMarker from './CustomMarker';
-import EditScreenInfo from './EditScreenInfo';
 import Listing from './Listing';
-import { MonoText } from './StyledText';
-import { Text, TextProps, useThemeColor, View, ViewProps } from './Themed';
 
-export {
-  CustomMarker,
-  EditScreenInfo,
-  Listing,
-  MonoText,
-  Text,
-  TextProps,
-  useThemeColor,
-  View,
-  ViewProps,
-};
+export { CustomMarker, Listing };
