8ecbc8df0a84460643cc27f81e103b981bb9fbcf
Enrique <sotomaque@gmail.com>
2021-04-01 19:50:45 -0700

    feat: ref between carousel and selected map item

--- src/screens/SearchResultsMapScreen/SearchResultsMapScreen.tsx
+++ src/screens/SearchResultsMapScreen/SearchResultsMapScreen.tsx
@@ -1,14 +1,37 @@
 import { CustomMarker, ListingCarouselItem } from '@components';
-import React, { useState } from 'react';
-import { FlatList, useWindowDimensions, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { FlatList, useWindowDimensions, View, ViewToken } from 'react-native';
 import MapView from 'react-native-maps';
 import places from '../../../assets/data/feed';
 import styles from './styles';
 
+type ViewableChangedPropsType = {
+  viewableItems: ViewToken[];
+};
+
 const SearchResultsMapScreen = () => {
   const [selectedPlaceId, setSelectedPlaceId] = useState<number | null>(null);
+  const flatListRef = useRef(null);
+  const viewConfig = useRef({ itemVisiblePercentThreshold: 70 });
 
+  const onViewChanged = useRef(
+    ({ viewableItems }: ViewableChangedPropsType) => {
+      if (viewableItems.length > 0) {
+        const selectedPlace = viewableItems[0].item;
+        setSelectedPlaceId(selectedPlace.id);
+      }
+    }
+  );
   const width = useWindowDimensions().width;
+
+  useEffect(() => {
+    if (setSelectedPlaceId === null || flatListRef === null) return;
+
+    // get index
+    const index = places.findIndex(place => place.id === selectedPlaceId);
+    // update ref to scroll to index
+    index !== -1 && flatListRef?.current?.scrollToIndex({ index });
+  }, [selectedPlaceId]);
 
   return (
     <View>
@@ -33,16 +56,21 @@ const SearchResultsMapScreen = () => {
         ))}
       </MapView>
 
-      {/* Listng Carousel */}
+      {/* Carousel */}
       <View style={styles.carouselContainer}>
         <FlatList
+          ref={flatListRef}
           data={places}
-          renderItem={({ item }) => <ListingCarouselItem listing={item} />}
+          renderItem={({ item, index }) => (
+            <ListingCarouselItem listing={item} />
+          )}
           horizontal
           showsHorizontalScrollIndicator={false}
           snapToInterval={width - 60}
           snapToAlignment='center'
           decelerationRate='fast'
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
